32e78d452e8debd22dedcdb302f685f3289e162f
myvictorlife <victorcmggg@gmail.com>
2022-10-23 14:44:25 +0200

    add firebase hosting

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app',
+  appName: 'admin',
   webDir: 'www',
   bundledWebRuntime: false
 };
